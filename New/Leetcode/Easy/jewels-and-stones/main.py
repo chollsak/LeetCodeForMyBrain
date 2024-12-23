03acbcd12e3fa7f0941a06c58eeab14670bf3f84
Chollasak Anuwareepong <chollsak@Chollasaks-MacBook-Air.local>
2024-12-23 12:53:29 +0700

    s

---
+++ New/Leetcode/Easy/jewels-and-stones/main.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        counter = 0
+        for i in jewels:
+            for j in stones:
+                if i == j:
+                    counter += 1
+        
+        return counter 
+
+
+s = Solution()
+print(s.numJewelsInStones("aA", "aAAbbbb"))
+        
